276785ff2b4524508eee089dad045c0cd78ff9a2
Victor Augusto <143808769+Victor-S4ntos@users.noreply.github.com>
2023-09-12 12:20:20 -0300

    Add files via upload
    Validando o botão de excluir e exibindo os valores das teclas no campo dinamicamente.

--- calculadora.js/main.js
+++ calculadora.js/main.js
@@ -1,36 +1,46 @@
+//classe calculadora -----------------------------------
 class Calculadora {
     constructor(valorA, valorB, operacao) {
-      this.valorA = valorA;
-      this.operacao = operacao;
-      this.valorB = valorB;
+      this._valorA = valorA;
+      this._operacao = operacao;
+      this._valorB = valorB;
     }
     
     conta() {
-      if(this.operacao === '+') {
-        return this.valorA + this.valorB 
-    
-      }else if (this.operacao === '-') {
-        return this.valorA - this.valorB
+      if(this._operacao === '+') {
+        return this._valorA + this._valorB 
       }
-
-      else if(this.operacao === 'x') {
-        return this.valorA * this.valorB
+      else if (this._operacao === '-') {
+        return this._valorA - this._valorB
       }
-
-      else if(this.operacao === '/') {
-        return this.valorA / this.valorB
+      else if(this._operacao === 'x') {
+        return this._valorA * this._valorB
+      }
+      else if(this._operacao === '/') {
+        return this._valorA / this._valorB
+      }
+      else {
+        alert('operador(es) inexistente(es)')
       }
     }
-  }      
+}
 
-const somar = new Calculadora (10, 10, "+")
-console.log("resultado da soma:", somar.conta());
+const contar = new Calculadora (20, 10, '+')
+console.log(contar, "  resultado da conta:", contar.conta())
 
-const subtrair = new Calculadora (10, 2, "-")
-console.log("resultado da subtração:", subtrair.conta());
+//exbir no campo -----------------
+const camposDeExibicao = document.querySelector('.display')
+const botoes = document.querySelectorAll('.botoes')
 
-const multiplicar = new Calculadora (10, 10, "x") 
-console.log("resultaod da multiplicação:", multiplicar.conta());
+botoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
 
-const dividir = new Calculadora (10, 2, "/")
-console.log("resultado da divisão:", dividir.conta())
+   const textoBotao = botao.innerHTML; 
+
+    camposDeExibicao.innerHTML += textoBotao; 
+  })
+})
+
+//limpar campo ----------------
+const botaoLimpar = document.getElementById('clear')
+botaoLimpar.addEventListener("click", () => {camposDeExibicao.innerHTML=""})
